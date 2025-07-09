e32cb45a519a6e2bc784d48c51f3ed1a90a0d477
RonaldoJB45 <ronaldojbetec@gmail.com>
2025-07-09 13:05:43 -0300

    ftp
    criado service para ftp

---
+++ source code/Core/MetroBackup.Infra.Acl/FtpService.cs
@@ -0,0 +1,46 @@
+﻿using MetroBackup.Domain.Entities;
+using MetroBackup.Domain.Interfaces;
+using System;
+using System.IO;
+using System.Net;
+
+namespace MetroBackup.Infra.Acl
+{
+    public class FtpService : IFtpService
+    {
+        public void Enviar(Configuracao configuracao)
+        {
+            var ftp = configuracao.Ftp;
+
+            string host = ftp.Host;
+            string user = ftp.User;
+            string password = ftp.Password;
+
+            string nomeArquivo = host + "/" + Guid.NewGuid().ToString();
+            string destino = "";
+
+            var ftpRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(nomeArquivo));
+            ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
+            ftpRequest.Proxy = null;
+            ftpRequest.UseBinary = true;
+            ftpRequest.Credentials = new NetworkCredential(user, password);
+
+            using (FileStream fs = File.OpenRead(destino))
+            {
+                using (Stream writer = ftpRequest.GetRequestStream())
+                {
+                    var buffer = new byte[1024 * 1024];
+                    int totalReadBytesCount = 0;
+                    int readBytesCount;
+
+                    while ((readBytesCount = fs.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        writer.Write(buffer, 0, readBytesCount);
+                        totalReadBytesCount += readBytesCount;
+                        var progress = totalReadBytesCount * 100.0 / fs.Length;
+                    }
+                }
+            }
+        }
+    }
+}
